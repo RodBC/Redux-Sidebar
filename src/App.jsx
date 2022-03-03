d0bf1d777f17009b23ecde029300ca7b7ed8cf24
RodBC <rodrigo.barros.castro@hotmail.com>
2022-03-03 14:57:47 -0300

    deleting files that are being directly imported through the redux store

--- src/App.jsx
+++ src/App.jsx
@@ -4,28 +4,6 @@ import { Video } from './Video';
 import { Provider } from 'react-redux';
 import { store } from './Store';
 
-const state = {
-    modules: [
-      {
-        id: 1,
-        title: 'iniciando com react',
-        lessons: [
-          { id:1, title: 'primeira aula'},
-          { id:2, title: 'segunda aula' }
-          ]
-        },
-        {
-          id: 2,
-          title: 'Aprendendo redux',
-          lessons: [
-            { id:1, title: 'terceira aula'},
-            { id:2, title: 'quarta aula' }
-          ]
-        }        
-    ]
-  }
-  const {modules} = state;
-  
 function App() {
     return(
         <div>  
